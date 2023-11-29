0c1c5c630dbe6fc6a2a6ed3ae7ae983246c6183b
Javier Pardo <antonioj_2803@hotmail.com>
2023-11-29 15:43:52 -0500

    Added more styles for parallax

--- src/js/components/parallax-gogreen.js
+++ src/js/components/parallax-gogreen.js
@@ -5,9 +5,10 @@ const goGreenImg = document.querySelector(".gogreen-section__mountain");
 export function Parallax() {
   document.addEventListener("scroll", () => {
     // let valueY = window.scrollY - goGreenSection.offsetTop;
-    let valueY = window.scrollY * 0.03;
-    goGreenImg.style.setProperty("--y-parallax", valueY + "px");
-    console.log(valueY);
-    console.log(window.scrollY - goGreenSection.offsetTop);
+    let valueY = window.scrollY * 0.12;
+    goGreenTitle.style.setProperty("--y-parallax", -valueY + "px");
+    // goGreenImg.style.setProperty("--y-parallax", valueY + "px");
+    console.log(goGreenTitle.offsetTop);
+    // console.log(window.scrollY - goGreenSection.offsetTop);
   });
 }
