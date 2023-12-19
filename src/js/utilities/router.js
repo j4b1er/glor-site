7d87b41fc02c1813dccfe20a89af7521f849289c
Javier Pardo <antonioj_2803@hotmail.com>
2023-12-18 20:33:27 -0500

    SPA working

--- src/js/utilities/router.js
+++ src/js/utilities/router.js
@@ -7,55 +7,62 @@ export const router = async () => {
   const routes = [
     {
       path: "/",
-      view: () => console.log("Vieweing Home"),
+      page: "/src/pages/index.html",
       title: "Innovetion and Hard work",
     },
     {
       path: "/about",
-      view: () => console.log("Vieweing About"),
+      page: "/src/pages/about.html",
       title: "Learn more about us",
     },
     {
       path: "/services",
-      view: () => console.log("Vieweing Services"),
+      page: "/src/pages/services.html",
       title: "What we offer",
     },
     {
       path: "/projects",
-      view: () => console.log("Vieweing Projects"),
+      page: "/src/pages/project.html",
       title: "Look at our portfolio",
     },
     {
       path: "/gogreen",
-      view: () => console.log("Vieweing Go Green"),
+      page: "/src/pages/gogreen.html",
       title: "The enviroment matter to us",
     },
     {
       path: "/404",
-      view: () => console.log("Vieweing Not Found"),
+      page: "/src/pages/error.html",
       title: "Page not found",
     },
   ];
 
-  const potentialMatches = routes.map((route) => {
-    return {
-      route,
-      isMatch: location.pathname === route.path,
-    };
-  });
+  //   const potentialMatches = routes.map((route) => {
+  //     return {
+  //       route,
+  //       isMatch: location.pathname === route.path,
+  //     };
+  //   });
 
-  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
+  //   let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch);
 
-  if (!match) {
-    match = {
-      route: routes[3],
-      isMatch: true,
-    };
-  }
+  //   if (!match) {
+  //     match = {
+  //       route: routes[3],
+  //       isMatch: true,
+  //     };
+  //   }
 
-  const routeFound = routes.find((route) => route.path === location.pathname);
+  //   console.log(routeFound);
 
-  console.log(routeFound.view());
+  const handlePage = async () => {
+    let routeFound = routes.find((route) => route.path === location.pathname);
+    if (!routeFound) routeFound = routes[5];
+    const pageFetch = await fetch(routeFound.page).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = pageFetch;
+  };
+
+  handlePage();
 
   //   console.log(match.route.view());
 };
