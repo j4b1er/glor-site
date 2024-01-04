22c0ecea3090a3055e64fb20a35431a2f1adc0a4
Javier Pardo <antonioj_2803@hotmail.com>
2024-01-03 22:22:08 -0500

    First tests with view transition api

--- src/js/utilities/router.js
+++ src/js/utilities/router.js
@@ -14,36 +14,43 @@ export async function router() {
       path: "/",
       page: "/src/pages/index.html",
       title: "Innovation and Hard work",
+      transition: false,
     },
     {
       path: "/about",
       page: "/src/pages/about.html",
       title: "Learn more about us",
+      transition: false,
     },
     {
       path: "/services",
       page: "/src/pages/services.html",
       title: "What we offer",
+      transition: true,
     },
     {
       path: "/services/isp-structural-cabling",
       page: "/src/pages/services/isp-structural-cabling.html",
       title: "IPS Structural Cabling Services",
+      transition: true,
     },
     {
       path: "/projects",
       page: "/src/pages/projects.html",
       title: "Look at our portfolio",
+      transition: false,
     },
     {
       path: "/go-green",
       page: "/src/pages/go-green.html",
       title: "The enviroment matter to us",
+      transition: false,
     },
     {
       path: "/404",
       page: "/src/pages/error.html",
       title: "Page not found",
+      transition: false,
     },
   ];
 
@@ -55,7 +62,20 @@ export async function router() {
   let pageFetch = await fetch(routeFound.page);
   let data = await pageFetch.text();
   document.title = `${routeFound.title} | Glor`;
-  document.getElementById("main-page").innerHTML = data;
+  // document.getElementById("main-page").innerHTML = data;
+
+  if (routeFound.transition) {
+    if (!document.startViewTransition) {
+      document.getElementById("main-page").innerHTML = data;
+    } else {
+      document.startViewTransition(() => {
+        document.getElementById("main-page").innerHTML = data;
+        // ContentComponents();
+      });
+    }
+  } else {
+    document.getElementById("main-page").innerHTML = data;
+  }
 }
 
 window.addEventListener("popstate", () => {
